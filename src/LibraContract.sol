60bc017e326da8b71d6059a01a7fcf181643f0cc
maple <393741062@qq.com>
2023-09-29 00:23:35 +0800

    forge install: forge-std

---
+++ src/LibraContract.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.20;
+
+import "forge-std/console.sol";
+// import { Ownable } from "openzeppelin/contracts/access/Ownable.sol";
+import { ECDSA } from "openzeppelin-contracts/utils/cryptography/ECDSA.sol";
+
+contract LibraContract {
+    // using ECDSA for bytes32;
+    address public admin;
+    address public signer;
+
+    error InvalidSignature();
+
+    enum OrderStatus {Paid, Shipped, Completed, Cancelled}
+
+    struct Order {
+        string id;
+        address buyer;
+        address seller;
+        uint256 price;
+        uint256 quantity;
+        uint256 payTime;
+        OrderStatus state;
+    }
+
+    mapping(string => Order) public orders;
+    mapping(string => uint) public deposits;
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    function createOrder(string memory id, address buyer, address seller, uint price, uint quantity,
+    bytes memory signature) public payable {
+        // Check if msg.value is equal to totalPrice
+        uint256 totalPrice = quantity * price;
+        require(msg.value == totalPrice, "Value sent is not correct");
+
+        bytes32 hashedParams = keccak256(
+            abi.encodePacked(id, buyer, seller, price, quantity)
+        );
+        // bytes32 ethSignedMessageHash = getEthSignedMessageHash(hashedParams);
+        address recovered = ECDSA.recover(hashedParams, signature);
+        if (recovered != signer) revert InvalidSignature();
+        console.logAddress(recovered);
+
+        uint256 payTime = block.timestamp;
+        orders[id] = Order({
+            id: id,
+            buyer: buyer,
+            seller: seller,
+            price: price,
+            payTime: payTime,
+            quantity: quantity,
+            state: OrderStatus.Paid
+        });
+    }
+
+    function getEthSignedMessageHash(
+        bytes32 _messageHash
+    ) public pure returns (bytes32) {
+        return
+            keccak256(
+                abi.encodePacked("\x19Ethereum Signed Message:\n32", _messageHash)
+            );
+    }
+
+    function setSigner(address _signer) public {
+        signer = _signer;
+    }
+
+    function getOrder(string memory id) public view returns(Order memory) {
+        return orders[id];
+    }
+
+    function getOrderId(string memory id) public view returns(string memory) {
+        return orders[id].id;
+    }
+
+    function getOrderBuyer(string memory id) public view returns(address) {
+        return orders[id].buyer; 
+    }
+
+    function cancelOrder(uint id) public {}
+
+    function shipOrder(uint id) public {}
+}
--- test/Foo.t.sol
+++ test/Foo.t.sol
@@ -1,57 +1,58 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.19 <0.9.0;
+pragma solidity >=0.8.20;
 
-import { PRBTest } from "@prb/test/PRBTest.sol";
-import { console2 } from "forge-std/console2.sol";
-import { StdCheats } from "forge-std/StdCheats.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import { LibraContract } from "../src/LibraContract.sol";
+import { ECDSA } from "openzeppelin-contracts/utils/cryptography/ECDSA.sol";
 
-import { Foo } from "../src/Foo.sol";
+contract LibraContractTest is Test {
+    using ECDSA for bytes32;
+    enum OrderStatus {Paid, Shipped, Completed, Cancelled}
+    struct Order {
+        string id;
+        address buyer;
+        address seller;
+        uint256 price;
+        uint256 quantity;
+        uint256 payTime;
+        OrderStatus state;
+    }
+    LibraContract _libra;
 
-interface IERC20 {
-    function balanceOf(address account) external view returns (uint256);
-}
-
-/// @dev If this is your first time with Forge, read this tutorial in the Foundry Book:
-/// https://book.getfoundry.sh/forge/writing-tests
-contract FooTest is PRBTest, StdCheats {
-    Foo internal foo;
-
-    /// @dev A function invoked before each test case is run.
-    function setUp() public virtual {
-        // Instantiate the contract-under-test.
-        foo = new Foo();
+    function setUp() public {
+        _libra = new LibraContract();
     }
 
-    /// @dev Basic test. Run it with `forge test -vvv` to see the console log.
-    function test_Example() external {
-        console2.log("Hello World");
-        uint256 x = 42;
-        assertEq(foo.id(x), x, "value mismatch");
+    function testCreateOrder() public {
+        _libra.setSigner(0x19A6acE647842f55F6DF65973f72bfB298398c2c);
+        // 模拟下单数据
+        string memory id = "order1";
+        address buyer = address(0xf3efBad1f23b25B0941Ba114f0f185718BaE0375); 
+        address seller = address(0x19A6acE647842f55F6DF65973f72bfB298398c2c);
+        uint price = 1 ether;
+        uint quantity = 2;
+
+        // 模拟签名
+        uint256 privateKey = 0x4208f1cfd43f87cad512ee1163b8f96f13e631174619f6ad81b7acc4298444b2;
+
+        bytes32 hashed = keccak256(abi.encodePacked(id, buyer, seller, price, quantity));
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, hashed);
+        bytes memory signature = abi.encodePacked(r, s, v);
+        console.logBytes(signature);
+        // 下单
+        // vm.startPrank(buyer);
+        _libra.createOrder{value: price * quantity}(
+            id, buyer, seller, price, quantity, signature
+        );
+
+        address order = _libra.getOrderBuyer(id);
+        assertEq(order, buyer);
     }
 
-    /// @dev Fuzz test that provides random values for an unsigned integer, but which rejects zero as an input.
-    /// If you need more sophisticated input validation, you should use the `bound` utility instead.
-    /// See https://twitter.com/PaulRBerg/status/1622558791685242880
-    function testFuzz_Example(uint256 x) external {
-        vm.assume(x != 0); // or x = bound(x, 1, 100)
-        assertEq(foo.id(x), x, "value mismatch");
-    }
+    // function testSetSigner() public {
+    // }
 
-    /// @dev Fork test that runs against an Ethereum Mainnet fork. For this to work, you need to set `API_KEY_ALCHEMY`
-    /// in your environment You can get an API key for free at https://alchemy.com.
-    function testFork_Example() external {
-        // Silently pass this test if there is no API key.
-        string memory alchemyApiKey = vm.envOr("API_KEY_ALCHEMY", string(""));
-        if (bytes(alchemyApiKey).length == 0) {
-            return;
-        }
-
-        // Otherwise, run the test against the mainnet fork.
-        vm.createSelectFork({ urlOrAlias: "mainnet", blockNumber: 16_428_000 });
-        address usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-        address holder = 0x7713974908Be4BEd47172370115e8b1219F4A5f0;
-        uint256 actualBalance = IERC20(usdc).balanceOf(holder);
-        uint256 expectedBalance = 196_307_713.810457e6;
-        assertEq(actualBalance, expectedBalance);
-    }
+    // function testCancelOrder() public {
+    // }
 }
