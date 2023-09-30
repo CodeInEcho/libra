a6393b6c479ece23a78cd7238384e34adb601881
maple <393741062@qq.com>
2023-09-30 13:30:49 +0800

    feat: Add collateral

--- src/LibraContract.sol
+++ src/LibraContract.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.19;
+pragma solidity >=0.8.20;
 
 import "forge-std/console.sol";
 // import { Ownable } from "openzeppelin/contracts/access/Ownable.sol";
@@ -18,23 +18,30 @@ contract LibraContract {
         string id;
         address buyer;
         address seller;
+        uint256 amount;
         uint256 price;
         uint256 quantity;
         uint256 payTime;
+        uint256 feesRatio;
+        uint256 collateral;
         OrderStatus state;
     }
 
     mapping(string => Order) public orders;
     mapping(string => uint) public deposits;
+    // Track status of each order (validated, cancelled, and fraction filled).
+    mapping(string => OrderStatus) private _orderStatus;
 
     constructor() {
         admin = msg.sender;
     }
 
-    function createOrder(string memory id, address buyer, address seller, uint price, uint quantity,
-    bytes memory signature) public payable {
+    function createOrder(string memory id, address buyer, address seller, uint256 price, uint256 quantity, 
+    uint256 collateral, uint256 feesRatio, bytes memory signature) public payable {
+        require(msg.sender == buyer, "Invalid buyer");
+
         // Check if msg.value is equal to totalPrice
-        uint256 totalPrice = quantity * price;
+        uint256 totalPrice = quantity * price + collateral;
         require(msg.value == totalPrice, "Value sent is not correct");
 
         bytes32 hashedParams = keccak256(
@@ -46,13 +53,17 @@ contract LibraContract {
         console.logAddress(recovered);
 
         uint256 payTime = block.timestamp;
+
         orders[id] = Order({
             id: id,
             buyer: buyer,
             seller: seller,
             price: price,
+            amount: quantity * price,
             payTime: payTime,
             quantity: quantity,
+            feesRatio: feesRatio,
+            collateral: collateral,
             state: OrderStatus.Paid
         });
     }
@@ -79,7 +90,11 @@ contract LibraContract {
     }
 
     function getOrderBuyer(string memory id) public view returns(address) {
-        return orders[id].buyer; 
+        return orders[id].buyer;
+    }
+
+    function getOrderStatus(string memory id) public view returns(OrderStatus) {
+        return orders[id].status;
     }
 
     function cancelOrder(uint id) public {}
